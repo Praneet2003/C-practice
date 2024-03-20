3a356469d6402f60267c1c2c9c7bf23488161232
Praneet2003 <rajpraneet4@gmail.com>
2024-03-20 21:50:16 +0530

    Without_return_type & without Arguments

---
+++ Functions/without_return_type & without Arguments.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void add();
+void add(){
+    int x,y,z;
+    scanf("%d %d",&x,&y);
+    z =x+y;
+    printf("The Result is : %d",z);
+}
+int main(){
+    int c;
+    add();
+}
+// Output:-
+// if x = 10  and y =40
+// /tmp/eNQTD6jJZq.o
+// The Result is : 50
+
+// === Code Execution Successful ===
