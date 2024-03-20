0ee1a7219cf6ea19857f655bab31474e05e50273
Praneet2003 <rajpraneet4@gmail.com>
2024-03-20 21:48:53 +0530

    Added without_Return_type & with Arguments

---
+++ Functions/without_Return_type & with Arguments.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+void add(int x, int y);
+void add(int x, int y){
+    int z;
+    z=x+y;
+    printf("Result is %d",z);
+}
+int main(){
+    int a,b,c;
+    scanf("%d %d",&a,&b);
+    void add(a,b);
+}
+//Output:-
+// if a = 10 and b =20
+// /tmp/rHtYbvATJY.o
+// Result is 30
+
+// === Code Execution Successful ===
