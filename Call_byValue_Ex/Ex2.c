73b5628fe3fff0171c1833aae891128e6ec654ca
Praneet2003 <rajpraneet4@gmail.com>
2024-03-24 00:01:36 +0530

    VVI Examples of Call_By_Value

--- Call_byValue_Ex/Ex2.c
+++ Call_byValue_Ex/Ex2.c
@@ -11,6 +11,6 @@ int main(){
     scanf("%d %d %d",&a,&b,&c);
     printf("%d %d %d\n",a,b,c);
     shift(&a,&b,&c);
-    printf("%d %d %d",a,b,c);
+    printf("%d %d %d are",a,b,c);
     return 0;
 }
