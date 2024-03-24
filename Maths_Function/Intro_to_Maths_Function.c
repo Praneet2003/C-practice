38475732384dec5135554c80bd0964d4a6a44983
Praneet2003 <rajpraneet4@gmail.com>
2024-03-24 20:50:36 +0530

    Intro to Maths Function

---
+++ Maths_Function/Intro_to_Maths_Function.c
@@ -0,0 +1,24 @@
+// You are using GCC
+//Maths Function
+/*1:)*/ floor() //=> It return the nearest integer, which is less than or equal to argument passed to this function
+//Ex:- floor(4.6) = 4
+//
+/*2:)*/ Ceil() //=> it returns the nearest integer, which is greater than or equal to argument passed to the function
+//Ex:- ceil(4.6) = 5
+//
+/*3:)*/sqrt() //=> it return the squar root of the passed argument in this function.
+//Ex:- sqrt(16) = 4
+//
+/*4:)*/ pow() //=> it return the power of the first argument with respect to the second argument passed to this function.
+//Ex:- pow(2,3) = 2**3 = 8
+/*5:)*/ abs() //=> it returns the absolute value of the passed arguments.
+//NOTE:- absolute values are always positive/
+//Ex:- abs(-4) =4
+/*6:)*/ trunc() //=> it trucates decimal value from the floating point value and return integer part.
+//* Double, float, long Double are valid arguments to be passed in this function.
+//Ex:- trunc(4.899) = 4.000
+/*7:)*/ round() //=> Returns the nearest integer value of float/double/long double arguments passed to this function
+//Betwwen(0-0.5) => round of to the nearest small int 
+//Ex:- round(4.5) = 4.00
+//Between (0.5-0.9) => round off to the nearest greater integer
+//Ex:- round(4.8) = 5.00  
