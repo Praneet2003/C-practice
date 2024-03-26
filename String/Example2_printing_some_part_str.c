6ae34b95aa1151bfd7b5a9c63f2fe34a1a2df591
Praneet2003 <rajpraneet4@gmail.com>
2024-03-26 19:34:40 +0530

    Example for printing some part of string

---
+++ String/Example2_printing_some_part_str.c
@@ -0,0 +1,12 @@
+//you have to print some part of string.....
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[5]="Quiz";
+    printf("%s",str+1);
+    printf("%s",str+2);
+    return 0;
+}
+//Output
+//uiz
+// iz
