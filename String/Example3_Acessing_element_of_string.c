b1be4c82655ba794b51da3a517de9121591b3db0
Praneet2003 <rajpraneet4@gmail.com>
2024-03-26 19:33:52 +0530

    Example for Acessing element of an string

---
+++ String/Example3_Acessing_element_of_string.c
@@ -0,0 +1,12 @@
+//Acessing the element of an string using indexing.
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[5] = "Quiz";
+    printf("%c",str[0]);
+    printf("%c",str[1]);
+    return 0;
+}
+//Output:-
+//Q
+//u
