6f716f20639bfd2386b4cfea2b87dd714666ccf5
Praneet2003 <rajpraneet4@gmail.com>
2024-03-27 13:46:38 +0530

    Structure Data type

---
+++ User_Defined_Data_types/Structure_datatype/structure_datatype.c
@@ -0,0 +1,19 @@
+//Structure Data type:-
+//Structure is a used defied data types which allows us to compile data of different data types
+// Collection of logically related data
+//
+//Syntax:-
+#include<stdio.h>
+// struct [structure_tag]{}
+
+struct structure_tag
+{
+    /* data */
+    /*datatype members;*/
+    int member1;
+    char Name[50];
+    int registration;
+    float fees;
+};
+//Here for all type of data we are using variable, 
+//but in place of these variables we can use single structure variable 
