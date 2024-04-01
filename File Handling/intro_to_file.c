55caa8e5c66d45fa5492b27d55bd3397b714fcf3
Praneet2003 <rajpraneet4@gmail.com>
2024-04-01 21:45:50 +0530

    Introduction to file

---
+++ File Handling/intro_to_file.c
@@ -0,0 +1,23 @@
+//File:-
+// A flie is a container in seconadry storage devices, it stores data permanently.
+//Need of Flie handling:-
+// To move data eaisly from one device to another.
+// To provide security to data.
+// To store frequently used large amount of data.
+//
+//Types of files:-
+//           1:) Text file(.txt)               2:) Binary file(.bin)
+//
+//1=> Text File:- 
+// *it contains plain text, can be created using simple text editor such as notepad
+// *Easy to maintaint(eaisy deletion, updations).
+// *Provides least security an takes more space.
+// *eaisy Readable.
+//
+//2=> Binary File:-
+// *it stores data in binary form i.e 0 or 1
+// *it can Store more data,provides better security.
+// *Not Eaisly Readable.
+
+
+
