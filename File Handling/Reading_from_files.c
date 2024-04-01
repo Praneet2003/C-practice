2d0ba53a6cf4529e08a63aaf2c66b6d5e71d37c6
Praneet2003 <rajpraneet4@gmail.com>
2024-04-01 21:49:10 +0530

    Reading from a file

---
+++ File Handling/Reading_from_files.c
@@ -0,0 +1,15 @@
+// # Reading from file- 
+//Studio library provides thue fundi to read from a file Those functions are:-
+//
+//1:) fgetc(file_pointen):- 
+// It seturns the next characten from the file pointed to
+// by the file pointer when the end of the file has been reached, the EOF is sent back.
+//
+//2:) fgets(buffer, n, file pointer):- 
+// It read n-1 characters from the file and stores the string in a buffetes
+// in which the NULL chasauter '10' is appended as the last character.
+//
+//3:) fscanf(file pointers, conversion_specifere_variable_address):- 
+// used to parse & analyse data. It reads                                      
+// Charter from the file and assigns the imput to a list of variables using comersion specifurs. 
+// fsanf stops reading a string when spare or necoline is encountered.
