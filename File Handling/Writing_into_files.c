2a3728100706a1a0f3d28e4cea071f2e155e983c
Praneet2003 <rajpraneet4@gmail.com>
2024-04-01 21:48:36 +0530

    Writing into file

---
+++ File Handling/Writing_into_files.c
@@ -0,0 +1,16 @@
+//Writing into files:-
+// stdio library provides there functions to wilde file.
+//These function are:-
+// fputs (char, fil-pomter) 
+// fputs (str, bile potantes)
+// fprint (file printer, format specifier, verdeble);
+//
+// (1) spute (char, file pointen):- 
+//   *Id writes. charactell to a bile pointed to by bite file pointer.
+//
+// (2) fputs(str, bile pomter):- 
+//   *It usites a string to the file pointed to by file pointer.
+//
+// (3) fprint (file pontes, str, variable lists):- 
+//  *It prints a sting to the file pointed to by file pointer the string can 
+//  *optionally include format specifiers and a list of varible variable_list.
