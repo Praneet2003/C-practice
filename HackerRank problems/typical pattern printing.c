9e3d40481809072d84bff1626abf9c94c652d201
Praneet2003 <rajpraneet4@gmail.com>
2024-05-11 22:10:54 +0530

    Important pattern printing

---
+++ HackerRank problems/typical pattern printing.c
@@ -0,0 +1,39 @@
+// Sample Input:-
+// 7
+// Sample Output:-
+// 7 7 7 7 7 7 7 7 7 7 7 7 7 
+// 7 6 6 6 6 6 6 6 6 6 6 6 7 
+// 7 6 5 5 5 5 5 5 5 5 5 6 7 
+// 7 6 5 4 4 4 4 4 4 4 5 6 7 
+// 7 6 5 4 3 3 3 3 3 4 5 6 7 
+// 7 6 5 4 3 2 2 2 3 4 5 6 7 
+// 7 6 5 4 3 2 1 2 3 4 5 6 7 
+// 7 6 5 4 3 2 2 2 3 4 5 6 7 
+// 7 6 5 4 3 3 3 3 3 4 5 6 7 
+// 7 6 5 4 4 4 4 4 4 4 5 6 7 
+// 7 6 5 5 5 5 5 5 5 5 5 6 7 
+// 7 6 6 6 6 6 6 6 6 6 6 6 7 
+// 7 7 7 7 7 7 7 7 7 7 7 7 7 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	// Complete the code to print the pattern.
+    int l=n+(n-1);
+    for (int i = 0; i < l; i++) {
+        for (int j = 0; j < l; j++) {
+            int min = i < j ? i : j;
+            min = min < l - i ? min : l- i - 1;
+            min = min < l - j - 1 ? min : l - j - 1;
+            printf("%d ", n - min);
+        }
+        printf("\n");
+    }
+    return 0;
+}
